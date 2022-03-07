d705a448cfba2fc184b63a1aa32732241a9f5808
Adrienlms <adrien.lemoineseronie@ecole-isitech.fr>
2022-03-07 10:06:31 +0100

    add navbar

---
+++ react-1/src/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <div className="topnav">
+            <a className="active" href="#Home">Home</a>
+            <a href="#Proils">Profils</a>
+            <a href="#User">User</a>
+            <a href="#Liste">Liste</a>
+        </div>
+    )
+}
+export default Navbar;
